50cd92cc0f923b0c61132cafc36355bead332139
blackmore <nigel@blackmore.de>
2016-10-06 09:36:34 +0200

    first commit

---
+++ config/enviroment.rb
@@ -0,0 +1,24 @@
+
+ROOT = File.expand_path('../../', __FILE__)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# TEST PATHS - COMMENT OUT IN PRODUCTION
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+SOURCE_PATH = "#{ROOT}/test_folders/"
+PROCESSED_PATH = "#{ROOT}/test_folders/processed"
+TARGET_ONE = "#{ROOT}/test_folders/TARGET_ONE"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# TEST PATHS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# uncomments this to set the path of the folder you wish to watch
+# SOURCE_PATH = "path/to/source_files"
+
+# uncomments this to set the path where to place the sources once finished
+# PROCESSED_PATH = "path/to/processed_files"
+
+# uncomments these to set the target path of the watch folder for eztitles
+# TARGET_ONE = "path/to/TARGET_ONE"
+# TARGET_TWO = "path/to/TARGET_TWO"
